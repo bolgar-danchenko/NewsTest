88fd0347606af0f3aa30ea699861b231bb9cece1
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-02-16 15:37:16 +0400

    ProfileViewController layout created

--- NewsTest/Controller/ProfileViewController.swift
+++ NewsTest/Controller/ProfileViewController.swift
@@ -9,9 +9,78 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private lazy var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ProfileImage")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var nameLabel: PaddingLabel = {
+        let label = PaddingLabel()
+        label.text = "Name"
+        label.font = .systemFont(ofSize: 17)
+        label.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+        label.textAlignment = .left
+        label.paddingLeft = 10
+        label.layer.backgroundColor = CGColor(red: 0.459, green: 0.573, blue: 0.867, alpha: 0.1)
+        label.layer.cornerRadius = 20
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var emailLabel: PaddingLabel = {
+        let label = PaddingLabel()
+        label.text = "example@mail.ru"
+        label.font = .systemFont(ofSize: 17)
+        label.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
+        label.textAlignment = .left
+        label.paddingLeft = 10
+        label.layer.backgroundColor = CGColor(red: 0.459, green: 0.573, blue: 0.867, alpha: 0.1)
+        label.layer.cornerRadius = 20
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var signOutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.setTitle("Sign Out", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = UIColor(named: "Background")
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        view.addSubview(profileImage)
+        view.addSubview(nameLabel)
+        view.addSubview(emailLabel)
+        view.addSubview(signOutButton)
+        
+        NSLayoutConstraint.activate([
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            profileImage.widthAnchor.constraint(equalToConstant: 100),
+            profileImage.heightAnchor.constraint(equalToConstant: 100),
+            
+            nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 37),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            nameLabel.heightAnchor.constraint(equalToConstant: 44),
+            
+            emailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 16),
+            emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            emailLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            emailLabel.heightAnchor.constraint(equalToConstant: 44),
+            
+            signOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signOutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+        ])
+    }
 }
