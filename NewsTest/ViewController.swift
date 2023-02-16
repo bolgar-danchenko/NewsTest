648f6fe0abfa1d30c32935019027d9d7da99d9cd
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-02-16 11:52:47 +0400

    Initial Commit

---
+++ NewsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTest
+//
+//  Created by Konstantin Bolgar-Danchenko on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
