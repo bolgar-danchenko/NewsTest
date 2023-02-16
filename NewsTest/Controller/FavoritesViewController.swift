dd4cee08b58102d1cc99a41407a23522021bcb37
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-02-16 14:45:16 +0400

    MapViewController with custom pins created

---
+++ NewsTest/Controller/FavoritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesViewController.swift
+//  NewsTest
+//
+//  Created by Konstantin Bolgar-Danchenko on 16.02.2023.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
